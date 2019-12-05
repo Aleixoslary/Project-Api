33a53f5c97c5f7a5326d6bc67b0951d8b82e7677
woman <woman@info.local>
2019-12-04 21:52:52 -0300

    api-get

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,24 +4,49 @@ import Banner from '../components/banner';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
-
+import Api from '../services/Api'
 
 import '../assets/css/style.css'
 
-function Home() {
 
-  
+class Home extends React.Component{
+  constructor() {
+    super()
+    this.state={
+      listProducts:[]
+    }
+  }
 
+  getProducts = async () =>{
+    var response = await Api.get("/products");
+    console.log(response.data)
+    this.setState({listProducts : response.data.products});
+  }
+
+  render(){
   return (
     <div className="Home">
       <Header/>
        <Banner/>
+
        <h1 className="titleHome">Shop Now!!</h1>
       
+       <div className="products" >
+          {
+            this.state.listProducts.map((product, i)=>(
+              <div key={i} className="cardProducts">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+                <p>{product.description}</p>
+              </div>
+            )
+            )
+          }
+       </div>
       <Footer/>
     </div>
   );
-
+  }
 }
 
 export default Home;
